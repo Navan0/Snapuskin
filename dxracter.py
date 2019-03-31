380c19f874172bb4db821db1d16cf107d0daf42b
Your Name <nktclt@gmail.com>
2019-03-31 13:54:32 +0530

    images sorted

--- dxracter.py
+++ dxracter.py
@@ -1,11 +1,11 @@
 import json
 import os
 from pprint import pprint
-path, dirs, files = next(os.walk("/home/navaneeth/work/snapuskin/images/ISIC-images/testset"))
+path, dirs, files = next(os.walk("/home/navaneeth/work/snapuskin/mainset"))
 file_count = int(len(files)/2)
 for i in range(file_count):
     print(str(i))
-    with open('/home/navaneeth/work/snapuskin/images/ISIC-images/testset/ISIC_000000'+str(i)+'.json') as json_file:
+    with open('/home/navaneeth/work/snapuskin/mainset/ISIC_0000'+str('%03d' % i)+'.json') as json_file:
         data = json.load(json_file)
     name = str(data['meta']['clinical']['diagnosis'])
-    os.rename('/home/navaneeth/work/snapuskin/images/ISIC-images/testset/ISIC_000000'+str(i)'.jpg','/home/navaneeth/work/snapuskin/images/ISIC-images/'+str(name)+str(i)+'.jpg')
+    os.rename('/home/navaneeth/work/snapuskin/mainset/ISIC_0000'+str('%03d' % i)+'.jpg','/home/navaneeth/work/snapuskin/renamed/'+str(name)+str(i)+'.jpg')
